1c5d78542d18d171e10b8260eb06aa15d5c690b9
SleepySnorlax <glasssnorlax@gmail.com>
2022-10-13 21:26:56 -0500

    update naming
    renaming boilerplate to RSVP app

---
+++ contracts/Web3RSVP.sol
@@ -0,0 +1,55 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+contract Web3RSVP {
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+    }
+
+    mapping(bytes32 => CreateEvent) public idToEvent;
+
+    function createNewEvent(
+        uint256 eventTimestamp,
+        uint256 deposit,
+        uint256 maxCapacity,
+        string calldata eventDataCID
+    ) external {
+        // generate an eventID based on other things passed in to generate a hash
+        bytes32 eventId = keccak256(
+            abi.encodePacked(
+                msg.sender,
+                address(this),
+                eventTimestamp,
+                deposit,
+                maxCapacity
+            )
+        );
+
+        require(idToEvent[eventId].eventTimestamp == 0, "ALREADY REGISTERED");
+
+        address[] memory confirmedRSVPs;
+        address[] memory claimedRSVPs;
+
+
+        // this creates a new CreateEvent struct and adds it to the idToEvent mapping
+        idToEvent[eventId] = CreateEvent(
+            eventId,
+            eventDataCID,
+            msg.sender,
+            eventTimestamp,
+            deposit,
+            maxCapacity,
+            confirmedRSVPs,
+            claimedRSVPs,
+            false
+        );
+    }
+}
