fc9b26e9f8e2ceca924f79326371a47ea0c4849f
Cerpint4xt <rcatagrundy@gmail.com>
2022-11-09 02:08:31 +0000

    Adding Struct and mapping to the contract

--- contracts/Web3RSVP.sol
+++ contracts/Web3RSVP.sol
@@ -21,3 +21,20 @@ contract Greeter {
     }
 }
 */
+
+contract Web3RSVP {
+    struct CreateEvent {
+        bytes32 eventId;
+        string eventDataCID;
+        address eventOwner;
+        uint256 eventTimestamp;
+        uint256 deposit;
+        uint256 maxCapacity;
+        address[] confirmedRSVPs;
+        address[] claimedRSVPs;
+        bool paidOut;
+   }
+
+   mapping(bytes32 => CreateEvent) public idToEvent;
+   
+}
